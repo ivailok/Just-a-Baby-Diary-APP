af54d45952b949898b6f2bd4ed0ebd9a746cc1c7
Lina Chalakova <chalakovalina@gmail.com>
2013-08-31 16:11:44 +0300

    working on baby detaIs profile

--- JustABabyDiary/JustABabyDiary/pages/detailBabyProfile/detailBabyProfile.js
+++ JustABabyDiary/JustABabyDiary/pages/detailBabyProfile/detailBabyProfile.js
@@ -1,4 +1,5 @@
-﻿// For an introduction to the Page Control template, see the following documentation:
+﻿/// <reference path="../../js/viewModels.js" />
+// For an introduction to the Page Control template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkId=232511
 (function () {
     "use strict";
