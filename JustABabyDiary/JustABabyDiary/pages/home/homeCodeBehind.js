14a69f1f08f39a9df2edf697f58647b348def293
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-31 17:49:43 +0300

    working on add profile

--- JustABabyDiary/JustABabyDiary/pages/home/homeCodeBehind.js
+++ JustABabyDiary/JustABabyDiary/pages/home/homeCodeBehind.js
@@ -8,10 +8,15 @@
     }
 
     var goToProfileAddPage = function () {
+        var appBar = document.getElementById("home-app-bar").winControl;
+        if (appBar) {
+            appBar.hide();
+        }
         WinJS.Navigation.navigate("/pages/addProfile/addProfile.html");
     }
 
     WinJS.Utilities.markSupportedForProcessing(goToProfileDetailsPage);
+    WinJS.Utilities.markSupportedForProcessing(goToProfileAddPage);
 
     WinJS.Namespace.define("HomeCodeBehind", {
         callLoadProfiles: function () {
