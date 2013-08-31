9dde94dd6f768cf2c2f4796cd8d6dc02a0f558e9
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-31 16:16:30 +0300

    home updated

--- JustABabyDiary/JustABabyDiary/pages/home/home.js
+++ JustABabyDiary/JustABabyDiary/pages/home/home.js
@@ -1,11 +1,19 @@
-﻿(function () {
+﻿/// <reference path="homeCodeBehind.js" />
+
+(function () {
     "use strict";
 
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
+        init: function (element, options)
+        {
+            HomeCodeBehind.callLoadProfiles();
+        },
+
         ready: function (element, options) {
             // TODO: Initialize the page here.
+
         }
     });
 })();
--- JustABabyDiary/JustABabyDiary/pages/home/homeCodeBehind.js
+++ JustABabyDiary/JustABabyDiary/pages/home/homeCodeBehind.js
@@ -1,1 +1,19 @@
-﻿
+﻿/// <reference path="../../js/viewModels.js" />
+
+(function () {
+    var goToProfileDetailsPage = function (invokeEvent) {
+        WinJS.Navigation.navigate("/pages/detailBabyProfile/detailBabyProfile.html", {
+            indexInComputersList: invokeEvent.detail.itemIndex
+        });
+    }
+
+    WinJS.Utilities.markSupportedForProcessing(goToProfileDetailsPage);
+
+    WinJS.Namespace.define("HomeCodeBehind", {
+        callLoadProfiles: function () {
+            ViewModels.Profiles.loadProfiles();
+        },
+
+        goToProfileDetailsPage: goToProfileDetailsPage
+    })
+})();
